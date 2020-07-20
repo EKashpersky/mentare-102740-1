9fee07df06f0f1cc3798d05e21af7babd9a1f3f6
Yehor Kashperskyi <yehor.kashperskyi@viveapp.com>
2020-07-21 01:32:29 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// environment
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mentare-#102740-1';
+}
